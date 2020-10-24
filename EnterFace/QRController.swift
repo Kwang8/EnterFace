e56772b23c0de75955b3369a90a891ddfb6dbe95
Kevin <kev_wang@berkeley.edu>
2020-10-24 15:59:09 -0400

    2nd commit cleaned up qr code

--- EnterFace/QRController.swift
+++ EnterFace/QRController.swift
@@ -9,21 +9,47 @@
 import Foundation
 import UIKit
 import AVFoundation
+import Firebase
 
 class QRController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     var session: AVCaptureSession!
     var preview: AVCaptureVideoPreviewLayer!
-    
+    var ref: DatabaseReference!
+    var qrCodeFrameView: UIView?
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.black
         session = AVCaptureSession()
+        //firebase test
+        /*Database.database().reference().setValue(["12345",4])
+        var ref = Database.database().reference().child("room1")
+        ref.observeSingleEvent(of: .childAdded, with: { (snapshot) in
+             if let Dict = snapshot.value as? [String:Any] {
+                  //Do not cast print it directly may be score is Int not string
+                print(Dict["Capacity"] ?? "none")
+                print(Dict["Max"] ?? "none")
+             }
+        })
+        ref = Database.database().reference()
+        ref.observeSingleEvent(of: .childAdded, with: { (snapshot) in
+             if let Dict = snapshot.value as? [String:Any] {
+                  //Do not cast print it directly may be score is Int not string
+                print(Dict["1234"] ?? "none")
+             }
+        })*/
+        qrCodeFrameView = UIView()
+
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 10
+            view.addSubview(qrCodeFrameView)
+            view.bringSubviewToFront(qrCodeFrameView)
+        }
         
         guard let captureDevie = AVCaptureDevice.default(for: .video) else {
             return
         }
         let input: AVCaptureDeviceInput
-        
         do {
             input = try AVCaptureDeviceInput(device: captureDevie)
         }
@@ -41,7 +67,7 @@ class QRController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         if(session.canAddOutput(metadataOutput)) {
             session.addOutput(metadataOutput)
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            metadataOutput.metadataObjectTypes = [.qr]
+            metadataOutput.metadataObjectTypes = [.qr, .ean8, .ean13, .pdf417]
         }
         else {
             failCatch()
@@ -76,16 +102,18 @@ class QRController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
             session.stopRunning()
         }
     }
-    func metadata(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         session.stopRunning()
         if let metadataObject = metadataObjects.first {
             guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
             guard let stringValue = readableObject.stringValue else { return }
             AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            
+            let barCodeObject = preview?.transformedMetadataObject(for: metadataObjects[0] as! AVMetadataMachineReadableCodeObject)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            view.bringSubviewToFront(qrCodeFrameView!)
             result(code: stringValue)
         }
-        
-        dismiss(animated:true)
     }
     func result(code: String) {
         print(code)
